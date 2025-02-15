867027cd0deb669dcbc7a66825c8eddd9709363b
Cyriel Maningo <92829-22200943@SERVER2.DOMAIN>
2025-02-15 15:06:53 +0800

    0215_15_Understanding Database Seeders

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -34,7 +34,8 @@ class UserFactory extends Factory
     }
 
     /**
-     * Indicate that the model's email address should be unverified.
+     * Indicate that the model's email address should be unverified.*
+     * 
      */
     public function unverified(): static
     {
